a37c9e6010a67a792035922ad40f10e8a8f68100
echoflame14 <joshpeterson.dev@gmail.com>
2017-10-09 14:59:42 -0600

    added applet

---
+++ src/Day_1/HelloWorldApplet.java
@@ -0,0 +1,9 @@
+package Day_1;
+
+import java.awt.Graphics;
+
+public class HelloWorldApplet extends java.applet.Applet {
+    public void paint(Graphics g){
+        g.drawString("Hello World!", 5,25);
+    }
+}
